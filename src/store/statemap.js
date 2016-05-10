bc6599ebde70db001f00604f4b38c29df2129fb8
surfacew <surfacew@163.com>
2016-05-10 15:15:28 +0800

    chore(app): add app statemap to illustarte global state

---
+++ src/store/statemap.js
@@ -0,0 +1,31 @@
+/**
+ * stateMap
+ * this is only for demostration
+ * do not to import it
+ *
+ * @type {Object}
+ */
+
+const entityItem = {
+  name: 'JavaScript',
+  define: 'JavaScript is a good programming language.',
+  props: [{
+    name: 'author',
+    value: 'E.B.'
+  }]
+}
+
+const searchResultItem = {
+  name: 'JavaScript',
+  define: 'JavaScript is a good programming language.'
+}
+
+export const stateMap = {
+  searchText: 'JavaScript',
+
+  searchResults: [
+    searchResultItem
+  ],
+
+  entity: entityItem
+}
