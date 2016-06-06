ea19cb732a1701a8e27a8824831303e8cfac00d5
surfacew <surfacew@163.com>
2016-06-06 14:52:59 +0800

    feat(style): new ui and layout for the general app

--- src/routes/Search/components/SearchResultList.js
+++ src/routes/Search/components/SearchResultList.js
@@ -1,22 +1,24 @@
 import React from 'react'
 import _ from 'lodash'
 import { Card, CardHeader, CardText } from 'material-ui/Card'
+import classes from './SearchResultList.scss'
 
 export default (searchResults, gotoEntity) => (
   <div>
     {_.map(searchResults, (result) => {
       return (
-        <Card
-          key={result['_id']}
-          onClick={gotoEntity.bind(this, result['_id'], searchResults)}
-        >
-          <CardHeader
-            title={result.name}
-          />
-          <CardText>
-            {result.define.slice(0, 500)}
-          </CardText>
-        </Card>
+        <div className={classes.searchResultCard} key={result['_id']}>
+          <Card className={classes.searchResultCardContent}
+            onClick={gotoEntity.bind(this, result['_id'], searchResults)}
+          >
+            <CardHeader
+              title={result.name}
+            />
+            <CardText>
+              {result.define.slice(0, 500)}
+            </CardText>
+          </Card>
+        </div>
       )
     })}
   </div>
